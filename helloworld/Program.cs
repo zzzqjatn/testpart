cf3796639f52fa3f1f0d29fd7f9d1e669a55686c
zzzqjatn <zzzqjatn@naver.com>
2023-01-05 11:06:17 +0900

    [bsPark] style(Rular) modified

--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -2,6 +2,25 @@
 
 namespace helloworld
 {
+    // { 2023.01.05. Modified class position / Alpha
+    public class Ruler
+    {
+        private const float ONE_INCH = 2.54f;
+        private int Centimeter { get; set; } = 0;
+        public float Inch
+        {
+            get { return Centimeter * ONE_INCH; }
+            private set { Centimeter = (int)(value / ONE_INCH); }
+        }
+        public Ruler(int cmvalue) { Centimeter = cmvalue; }
+
+        public void Run()
+        {
+            Console.WriteLine($"{Centimeter}cm 는 {Inch}inch 입니다.");
+        }
+    } //    class ruler
+    // } 2023.01.05. Modified class position / Alpha
+
     internal class Program
     {
         static void Main(string[] args)
@@ -9,22 +28,5 @@ namespace helloworld
             Ruler ruler = new Ruler(10);
             ruler.Run();
         }
-
-        public class Ruler
-        {
-            private const float ONE_INCH = 2.54f;
-            private int Centimeter { get; set; } = 0;
-            public float Inch
-            {
-                get { return Centimeter * ONE_INCH; }
-                private set { Centimeter = (int)(value / ONE_INCH); }
-            }
-            public Ruler(int cmvalue) { Centimeter = cmvalue; }
-
-            public void Run()
-            {
-                Console.WriteLine($"{Centimeter}cm 는 {Inch}inch 입니다.");
-            }
-        } //    class ruler
     }
 }
