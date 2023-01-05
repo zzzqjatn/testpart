9e5d14effc28c15e3f241fac426de033238046e8
zzzqjatn <zzzqjatn@naver.com>
2023-01-05 09:45:54 +0900

    123

--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -6,7 +6,25 @@ namespace helloworld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Ruler ruler = new Ruler(10);
+            ruler.Run();
         }
+
+        public class Ruler
+        {
+            private const float ONE_INCH = 2.54f;
+            private int Centimeter { get; set; } = 0;
+            public float Inch
+            {
+                get { return Centimeter * ONE_INCH; }
+                private set { Centimeter = (int)(value / ONE_INCH); }
+            }
+            public Ruler(int cmvalue) { Centimeter = cmvalue; }
+
+            public void Run()
+            {
+                Console.WriteLine($"{Centimeter}cm 는 {Inch}inch 입니다.");
+            }
+        } //    class ruler
     }
 }
